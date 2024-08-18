61055eb2b59ad15cee33bc74f545b7f6bbf9ced9
TazmanS <tazmans123@gmail.com>
2024-08-18 18:22:57 +0300

    navbar

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,8 +1,7 @@
+import Dashboard from '@/app/dashboard/page'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world
-    </main>
+    <Dashboard/>
   );
 }
